ad34cb5b91880b6fb44dec3831664f68011de856
bbpp222006 <34976166+bbpp222006@users.noreply.github.com>
2021-03-07 11:52:30 +0800

    Update cdpc.py

--- utils/cdpc.py
+++ utils/cdpc.py
@@ -2,58 +2,50 @@ import urllib
 import http.cookiejar
 import json
 import os
+import requests
 
 def main():
-    ## 登录
-    account={
-        'email':os.environ['feiji_email'],
-        "passwd":os.environ['feiji_pass']
+    # 登录
+    account = {
+        'email': os.environ['feiji_email'],
+        "passwd": os.environ['feiji_pass']
     }
 
-    url = "https://dash.cdpc.fun/auth/login"
-    postdata =urllib.parse.urlencode({
+    url = "https://cdpc.fun/auth/login"
+    postdata = {
         'email': account['email'],
-        'passwd': account['passwd']
-    }).encode('utf-8')
-    header = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/32.0"
+        'passwd': account['passwd'],
+        'code': ""
     }
-    req = urllib.request.Request(url,postdata,header)
-    #自动记住cookie
-    cj = http.cookiejar.CookieJar()
-    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-    r = opener.open(req)
-    response=r.read().decode('utf-8')
-    logincheck=json.loads(response)
-    if logincheck['ret']!=1 :
+    session = requests.session()
+    session.headers.update(
+        {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0'})
+
+    req = session.post(url, data=postdata)
+    # 自动记住cookie
+    response = req.text.encode()
+    logincheck = json.loads(response)
+    if logincheck['ret'] != 1:
         print('登录失败')
     print('账号'+account['email']+'登录成功')
-    ## 签到
-    url = "https://dash.cdpc.fun/user/checkin"
-    postdata =urllib.parse.urlencode({}).encode('utf-8')
-    header = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/32.0",
-        "Content-Type":"application/json",
+    # 签到
+    url = "https://cdpc.fun/user/checkin"
+    postdata = {}
 
-    }
-    req = urllib.request.Request(url,postdata,header)
-    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-    r = opener.open(req)
-    response=r.read().decode('utf-8')
+    session.headers.update({"Content-Type": "application/json"})
+
+    req = session.post(url, data=postdata)
+    response = req.text
     try:
         ret = json.loads(response)
     except ValueError:
         print('签到失败')
         print('账号'+account['email']+'签到cdpc失败 msg:'+ret['msg'])
-    if ret['ret']!=1:
+    if ret['ret'] != 1:
         print('签到失败')
         print('账号'+account['email']+'签到cdpc失败 msg:'+ret['msg'])
     print(ret)
-    #登出
-    url = "https://dash.cdpc.fun/user/logout"
-    header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/32.0",
-    }
-    req = urllib.request.Request(url, None, header)
+    # 登出
+    url = "https://cdpc.fun/user/logout"
+    req = session.get(url)
     print("结束cdpc签到")
-
